0deac4da5c7debcccb643ff0d3cec02f672136ca
wwagner4 <wwagner4@gmail.com>
2017-08-29 08:48:08 +0200

    first word OK

--- src/main/scala/anagram/SentanceMorph.scala
+++ src/main/scala/anagram/SentanceMorph.scala
@@ -9,10 +9,8 @@ object SentanceMorph {
   def split(sentance: String): List[String] = sentance.split(" ").toList
 
   def morph(words: List[String]): List[String] =
-    extraWords(
-      interWords(
-        flipSpaces(words)
-      )
+    flipWords(
+      flipSpaces(words)
     )
 
   def flipSpaces(words: List[String]): List[String] = {
@@ -24,6 +22,7 @@ object SentanceMorph {
         (w1, w2)
       }
     }
+
     def flipSpaceLeft(w1: String, w2: String): (String, String) = {
       if (w2.length > minWordLen) {
         val x = w2.head
@@ -32,19 +31,19 @@ object SentanceMorph {
         (w1, w2)
       }
     }
+
     handlePairs(words)(flipSpaceLeft, flipSpaceRight)
   }
 
-  def interWords(words: List[String]): List[String] = words
-  def extraWords(words: List[String]): List[String] = words
+  def flipWords(words: List[String]): List[String] =
+    handlePairs(words, prob = 20)((a, b) => (b, a), (a, b) => (b, a))
 
-
-  def handlePairs[A](words: List[A])(f1: (A, A) => (A, A), f2: (A, A) => (A, A)): List[A] =
+  def handlePairs[A](words: List[A], prob: Int = 2)(f1: (A, A) => (A, A), f2: (A, A) => (A, A)): List[A] =
     words match {
       case Nil => Nil
       case w :: Nil => w :: handlePairs(Nil)(f1, f2)
       case w1 :: w2 :: rest =>
-        nextInt(2) match {
+        nextInt(prob) match {
           case 0 =>
             val (a, b) = f1(w1, w2)
             a :: handlePairs(b :: rest)(f1, f2)
@@ -60,13 +59,13 @@ object SentanceMorph {
     if (cnt == 0) Nil
     else {
       val m1: List[String] = morph(words)
-      m1 :: morph(m1, cnt -1)
+      m1 :: morph(m1, cnt - 1)
     }
   }
 
   def toRandom(sentance: String, cnt: Int): List[List[String]] = {
     val words: List[String] = split(sentance)
-    morph(words, cnt)
+    split(sentance) :: morph(words, cnt)
   }
 
 }
