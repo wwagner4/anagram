5be547baac72763c1a45507a908256a9c7b23720
wwagner4 <wwagner4@gmail.com>
2017-09-02 12:02:16 +0200

    further tests for assign ok

--- src/main/scala/anagram/impl/AnagramMorphLinear.scala
+++ src/main/scala/anagram/impl/AnagramMorphLinear.scala
@@ -4,5 +4,33 @@ import anagram.AnagramMorph
 
 class AnagramMorphLinear extends AnagramMorph {
 
-  def morph(from: String, to: String, lines: Int): List[String] = ???
+  def morph(from: String, to: String, lines: Int): List[String] = {
+    ???
+  }
+
+  def assign(a: String, b: String): Seq[Int] = {
+
+    def assign(al: List[Char], bl: List[Char], indexes: List[Int]): List[Int] = {
+
+      def firstUnusedIndex(char: Char, txt: List[Char], currentIndex: Int, usedIndexes: List[Int]): Int = {
+        txt match {
+          case Nil => throw new IllegalStateException(s"found no index for $char")
+          case head :: tail =>
+            if (char == head && !usedIndexes.contains(currentIndex)) currentIndex
+            else firstUnusedIndex(char, tail, currentIndex + 1, usedIndexes)
+        }
+      }
+
+      al match {
+        case Nil => indexes.reverse
+        case head :: tail =>
+          assign(tail, bl, firstUnusedIndex(head, bl, 0, indexes) :: indexes)
+      }
+    }
+
+
+    assign(a.toList, b.toList, List.empty[Int])
+  }
+
+
 }
--- src/test/scala/anagram/impl/LinearSuite.scala
+++ src/test/scala/anagram/impl/LinearSuite.scala
@@ -4,22 +4,25 @@ import org.scalatest.{FunSuite, MustMatchers}
 
 class LinearSuite extends FunSuite with MustMatchers {
 
-  val data = List(
+  val classUnderTest = new AnagramMorphLinear()
+
+  val dataOk = List(
     ("abc", "abc", Seq(0, 1, 2)),
     ("abc", "cba", Seq(2, 1, 0)),
     ("abc", "acb", Seq(0, 2, 1)),
-    ("abc", "cab", Seq(2, 0, 1)),
-    ("abc", "bac", Seq(1, 0, 2))
+    ("abc", "cab", Seq(1, 2, 0)),
+    ("abc", "bac", Seq(1, 0, 2)),
+    ("aaab", "aaab", Seq(0, 1, 2, 3)),
+    ("aaab", "baaa", Seq(1, 2, 3, 0)),
+    ("aabba", "abbaa", Seq(0, 3, 1, 2, 4)),
+    ("ditschi", "ihcstid", Seq(6, 0, 4, 3, 2, 1, 5)),
+    ("noah", "haon", Seq(3, 2, 1, 0)),
   )
 
-  for ((a, b, expected) <- data) {
+  for ((a, b, expected) <- dataOk) {
     test(s"assign $a $b") {
-      assign(a, b) must be(expected)
+      classUnderTest.assign(a, b) must be(expected)
     }
-  }
-
-  def assign(a: String, b: String): Seq[Int] = {
-    Seq(0, 1, 2)
   }
 
 }
