d1ac473c8637b16353bd74a202e46d8ad541cdb0
wwagner4 <wwagner4@gmail.com>
2017-09-16 23:46:10 +0200

    permutations OK

--- src/main/scala/anagram/solve/Solver.scala
+++ src/main/scala/anagram/solve/Solver.scala
@@ -10,10 +10,10 @@ import scala.collection.JavaConverters._
 object Solver {
 
   def solve(src: String, dict: Path): Stream[Iterable[String]] = {
-    val solver = new AnagramSolver(2, dict.toFile)
+    val solver = new AnagramSolver(3, dict.toFile)
     solver.findAllAnagrams(src.toLowerCase)
-      .asScala.map(_.asScala.toSet)
-      .iterator
+      .asScala.map(_.asScala.toList)
+      .flatMap(_.permutations)
       .toStream
   }
 
--- src/test/scala/anagram/solve/SolverTryout.scala
+++ src/test/scala/anagram/solve/SolverTryout.scala
@@ -9,7 +9,7 @@ object SolverTryout extends App {
   val dict1 = IoUtil.getTxtFilePathFromWorkDir("en01_dict")
   val dict2 = Paths.get(IoUtil.uri("wordlist/wordlist.txt"))
 
-  val re = Solver.solve("ingrid and bernd are in love", dict1)
+  val re = Solver.solve("bernd lives with ingrid in vienna", dict1)
 
   for ((sent, i) <- re.iterator.zipWithIndex) {
     println("%7d  -  %s".format(i, sent.mkString(" ")))
