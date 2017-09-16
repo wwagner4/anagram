31e93ad12d430f2018dc10448d4db7b2b9c8cf14
wwagner4 <wwagner4@gmail.com>
2017-09-16 19:39:45 +0200

    refactoring

--- src/test/scala/anagram/solve/SolverTryout.scala
+++ src/test/scala/anagram/solve/SolverTryout.scala
@@ -6,10 +6,10 @@ import anagram.common.IoUtil
 
 object SolverTryout extends App {
 
-  //val dict = IoUtil.getTxtFilePathFromWorkDir("en01_dict")
-  val dict = Paths.get(IoUtil.uri("wordlist/wordlist.txt"))
+  val dict1 = IoUtil.getTxtFilePathFromWorkDir("en01_dict")
+  val dict2 = Paths.get(IoUtil.uri("wordlist/wordlist.txt"))
 
-  val re = Solver.solve("ingrid and bernd are in love", dict)
+  val re = Solver.solve("ingrid and bernd are in love", dict1)
 
   for ((sent, i) <- re.iterator.zipWithIndex) {
     println("%7d  -  %s".format(i, sent.mkString(" ")))
