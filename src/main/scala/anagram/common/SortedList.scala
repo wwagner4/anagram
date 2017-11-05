c54ddf99786264b7b16827e0419ff9be1b8d1af2
wwagner4 <wwagner4@gmail.com>
2017-11-05 13:34:39 +0100

    save memory in Sorted List

--- src/main/scala/anagram/common/SortedList.scala
+++ src/main/scala/anagram/common/SortedList.scala
@@ -12,18 +12,26 @@ trait SortedList[T] {
 
 object SortedList {
 
+  val MAX = 10000
+
   def instance[T](implicit ord: Ordering[T]): SortedList[T] = {
     new SortedList[T] {
+
+      private var cnt = 0
 
       private var elems = Seq.empty[T]
 
       override def add(elem: T): Unit = synchronized {
-        elems = (elems :+ elem).sorted
+        cnt += 1
+        elems = (elems :+ elem).sorted.take(MAX)
       }
 
-      override def take(n: Int): Seq[T] = elems.take(n)
+      override def take(n: Int): Seq[T] = {
+        require(n <= MAX)
+        elems.take(n)
+      }
 
-      override def size: Int = elems.size
+      override def size: Int = cnt
 
     }
   }
