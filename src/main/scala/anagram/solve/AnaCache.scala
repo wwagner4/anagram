dc219837af00bb28af83eff7f94bcb8d0e2f9e72
wwagner4 <wwagner4@gmail.com>
2017-11-07 17:19:39 +0100

    limit ana cache

--- src/main/scala/anagram/solve/AnaCache.scala
+++ src/main/scala/anagram/solve/AnaCache.scala
@@ -7,7 +7,7 @@ class AnaCache {
   var map =  Map.empty[String, GenIterable[List[String]]]
 
   def addAna(txt: String, anas: GenIterable[List[String]]): Unit = synchronized {
-    map += (txt -> anas)
+    map = ((txt -> anas) :: map.toList).take(100000).toMap
   }
 
   def ana(txt: String): Option[GenIterable[List[String]]] = map.get(txt)
--- src/test/scala/anagram/Tryout.scala
+++ src/test/scala/anagram/Tryout.scala
@@ -2,6 +2,18 @@ package anagram
 
 object Tryout extends App {
 
-  2 to 10 foreach{i => val li = (0 to (100, 100 / i)).toList ; println(li) }
+  //2 to 10 foreach{i => val li = (0 to (100, 100 / i)).toList ; println(li) }
+
+  var m = Map(
+    ("A" -> 1),
+    ("C" -> 3),
+    ("B" -> 2),
+  )
+
+  m = (("R" -> 10) :: m.toList).take(4).toMap
+  m = (("X" -> 1) :: m.toList).take(4).toMap
+  m = (("Y" -> -3) :: m.toList).take(4).toMap
+  m = (("Z" -> 7) :: m.toList).take(4).toMap
+
 
 }
