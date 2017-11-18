ceee506dd33acee816190fc81c99ac3d24272616
wwagner4 <wwagner4@gmail.com>
2017-11-18 14:19:13 +0100

    removed warnings

---
+++ src/main/scala/anagram/ml/data/datamodel/bookwords/BookwordsMain.scala
@@ -0,0 +1,7 @@
+package anagram.ml.data.datamodel.bookwords
+
+object BookwordsMain extends App {
+
+    println("Not implemented")
+
+}
--- src/main/scala/anagram/ml/data/datamodel/bookwords/BookwordsMapper.scala
+++
@@ -1,9 +0,0 @@
-package anagram.ml.data.datamodel.bookwords
-
-import anagram.words.WordMapperFactory
-
-object BookwordsMapper extends WordMapperFactory {
-
-  override def create = ???
-
-}
