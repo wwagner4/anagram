05d0f6d28e681b16d78e8f125cbae21a22ea5f55
wwagner4 <wwagner4@gmail.com>
2017-12-04 00:04:08 +0100

    grm use only senteces with known words

--- src/main/scala/anagram/ml/data/common/SentenceRaterCounting.scala
+++ src/main/scala/anagram/ml/data/common/SentenceRaterCounting.scala
@@ -3,17 +3,20 @@ package anagram.ml.data.common
 class SentenceRaterCounting extends SentenceRater {
   override def rateSentence(sentences: Iterable[Sentence]): Iterable[Rated] = {
     val rmap: Seq[(Seq[String], Iterable[Sentence])] = sentences.groupBy(sent => sent.words).toSeq
-    for ((w, sentences) <- rmap) yield {
-      val y: Seq[(SentenceType, Int)] = sentences.map(_.sentenceType).groupBy(identity).mapValues(_.size).toSeq
-      val rating: Double = y.foldLeft(0.0) {
-        case (r, (stype, cnt)) => stype match {
-          case SentenceType_COMPLETE => r + cnt * 10
-          case SentenceType_BEGINNING => r + cnt * 5
-          case SentenceType_OTHER => r + cnt * 1
-          case SentenceType_RANDOM => throw new IllegalStateException("SentenceType_RANDOM makes no sense for Counting")
+    rmap.flatMap { case (w, sents) =>
+      if (w.contains("?")) None
+      else {
+        val y: Seq[(SentenceType, Int)] = sents.map(_.sentenceType).groupBy(identity).mapValues(_.size).toSeq
+        val rating: Double = y.foldLeft(0.0) {
+          case (r, (stype, cnt)) => stype match {
+            case SentenceType_COMPLETE => r + cnt * 10
+            case SentenceType_BEGINNING => r + cnt * 5
+            case SentenceType_OTHER => r + cnt * 1
+            case SentenceType_RANDOM => throw new IllegalStateException("SentenceType_RANDOM makes no sense for Counting")
+          }
         }
+        Some(Rated(Sentence(SentenceType_OTHER, w), rating))
       }
-      Rated(Sentence(SentenceType_OTHER, w), rating)
     }
   }
 }
