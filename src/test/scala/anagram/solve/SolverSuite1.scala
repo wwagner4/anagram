ae5e2795b78c67ebff5ad50feeaf45f6f7009e37
wwagner4 <wwagner4@gmail.com>
2017-12-30 10:57:22 +0100

    prepared new solver suite

--- src/test/scala/anagram/solve/SolverSuite1.scala
+++ src/test/scala/anagram/solve/SolverSuite1.scala
@@ -1,58 +1,166 @@
 package anagram.solve
 
+import anagram.gui.SolverFactoryPlain
+import anagram.ml.rate.RaterAi
+import anagram.model.Configurations
+import anagram.words.Wordlists
 import org.scalatest.{FunSuite, MustMatchers}
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 class SolverSuite1 extends FunSuite with MustMatchers {
 
   case class RE(rating: Double, words: Seq[String])
 
-  val expectedPlain1 = Seq(
-    ("wolfgang", "PLAIN_plain001", Seq(
-      RE(32.9957, List("flag", "gown")),
-      RE(33.4897, List("gown", "flag")))),
-    ("ditschi", "PLAIN_plain001", Seq(
-      RE(33.589, List("is", "ditch")),
-      RE(33.6183, List("hit", "disc")))),
-    ("ingrid bernd", "PLAIN_plain001", Seq(
-      RE(32.6135, List("in", "in", "beg", "dr", "dr")),
-      RE(32.6135, List("in", "in", "beg", "rr", "dd")))),
+  private implicit val exe: ExecutionContextExecutor = ExecutionContext.global
+
+  private val wlf = Wordlists.plainRatedLargeFine
+  private val baseSolver = SolverFactoryPlain(maxDepth = 5, parallel = 4, wlf).createSolver
+
+
+  private val expectedPlain = Seq(
+    ("ingrid bernd", Seq(
+      RE(31.630, List("in", "bred", "grind")),
+      RE(32.336, List("in", "grind", "bred")))),
+    ("wolfgang wagner", Seq(
+      RE(31.770, List("of", "grew", "gang", "lawn")),
+      RE(31.817, List("of", "grew", "lawn", "gang")))),
+    ("brigitte freimueller", Seq(
+      RE(31.359, List("little", "i", "free", "urge", "brim")),
+      RE(31.456, List("little", "i", "refuge", "re", "brim")))),
   )
 
-  val expectedPlainRand1 = Seq(
-    ("wolfgang", "PLAINR_plainRand001", Seq(
-      RE(22.10758, List("flag", "gown")),
-      RE(22.50175, List("gown", "flag")))),
-    ("ditschi", "PLAINR_plainRand001", Seq(
-      RE(22.657, List("is", "ditch")),
-      RE(22.65767, List("hit", "disc")))),
-    ("ingrid bernd", "PLAINR_plainRand001", Seq(
-      RE(21.4158, List("in", "in", "beg", "dr", "dr")),
-      RE(21.4158, List("in", "in", "beg", "rr", "dd")))),
+  for ((srcText, expSeq) <- expectedPlain) {
+    val cfg = Configurations.plain
+
+    val rdesc = cfg.cfgRaterAi.shortDescription
+    val desc = expSeq.flatMap(re => re.words).mkString(" ")
+    val rater = new RaterAi(cfg.cfgRaterAi.cfgRaterAi())
+    val anagrams: Seq[Ana] = SolverRatedImpl(baseSolver, rater).solve(srcText).toSeq
+    for (i <- 0 to 1) {
+      ignore(s"$rdesc $srcText $i $desc") {
+        val re = expSeq(i)
+        val ana = anagrams(i)
+        ana.sentence mustBe re.words
+        ana.rate mustBe re.rating +- 0.1
+      }
+    }
+  }
+
+  val expectedPlainRand = Seq(
+    ("wolfgang wagner", Seq(
+      RE(22.10758, List("of", "grew", "gang", "lawn")),
+      RE(22.50175, List("of", "grew", "lawn", "gang")))),
+    ("clint eastwood", Seq(
+      RE(22.657, List("and", "to", "is", "let", "cow")),
+      RE(22.65767, List("and", "to", "is", "cow", "let")))),
+    ("ingrid bernd", Seq(
+      RE(21.4158, List("in", "bred", "grind")),
+      RE(21.4158, List("in", "grind", "bred")))),
   )
 
-  val expectedGrm1 = Seq(
-    ("wolfgang", "GRM_grm001", Seq(
-      RE(0.56, List("flag", "gown")),
-      RE(0.52, List("gown", "flag")))),
-    ("ditschi", "GRM_grm001", Seq(
-      RE(0.50, List("is", "ditch")),
-      RE(0.49, List("hit", "disc")))),
-    ("ingrid bernd", "GRM_grm001", Seq(
-      RE(0.29, List("in", "in", "beg", "dr", "dr")),
-      RE(0.29, List("in", "in", "beg", "rr", "dd")))),
+  for ((srcText, expSeq) <- expectedPlainRand) {
+    val cfg = Configurations.plainRandom
+
+    val rdesc = cfg.cfgRaterAi.shortDescription
+    val desc = expSeq.flatMap(re => re.words).mkString(" ")
+    val rater = new RaterAi(cfg.cfgRaterAi.cfgRaterAi())
+    val anagrams: Seq[Ana] = SolverRatedImpl(baseSolver, rater).solve(srcText).toSeq
+    for (i <- 0 to 1) {
+      ignore(s"$rdesc $srcText $i $desc") {
+        val re = expSeq(i)
+        val ana = anagrams(i)
+        ana.sentence mustBe re.words
+        ana.rate mustBe re.rating +- 0.1
+      }
+    }
+  }
+
+  val expectedGrm = Seq(
+    ("wolfgang wagner", Seq(
+      RE(0.56, List("of", "grew", "gang", "lawn")),
+      RE(0.52, List("of", "grew", "lawn", "gang")))),
+    ("wolfgang brigitte", Seq(
+      RE(0.50, List("of", "it", "grew", "bag", "glint")),
+      RE(0.49, List("of", "it", "grew", "bang", "gilt")))),
+    ("ingrid bernd", Seq(
+      RE(0.29, List("in", "bred", "grind")),
+      RE(0.29, List("in", "grind", "bred")))),
   )
 
-  val expectedGrmRed1 = Seq(
-    ("wolfgang", "GRMRED_grmRed001", Seq(
-      RE(1.82, List("flag", "gown")),
-      RE(1.80, List("gown", "flag")))),
-    ("ditschi", "GRMRED_grmRed001", Seq(
-      RE(1.82, List("is", "ditch")),
-      RE(1.80, List("hit", "disc")))),
-    ("ingrid bernd", "GRMRED_grmRed001", Seq(
-      RE(1.71, List("in", "in", "beg", "dr", "dr")),
-      RE(1.71, List("in", "in", "beg", "rr", "dd")))),
+  for ((srcText, expSeq) <- expectedGrm) {
+    val cfg = Configurations.grammar
+
+    val rdesc = cfg.cfgRaterAi.shortDescription
+    val desc = expSeq.flatMap(re => re.words).mkString(" ")
+    val rater = new RaterAi(cfg.cfgRaterAi.cfgRaterAi())
+    val anagrams: Seq[Ana] = SolverRatedImpl(baseSolver, rater).solve(srcText).toSeq
+    for (i <- 0 to 1) {
+      ignore(s"$rdesc $srcText $i $desc") {
+        val re = expSeq(i)
+        val ana = anagrams(i)
+        ana.sentence mustBe re.words
+        ana.rate mustBe re.rating +- 0.1
+      }
+    }
+  }
+
+  val expectedGrmRed = Seq(
+    ("wolfgang wagner", Seq(
+      RE(1.82, List("of", "grew", "gang", "lawn")),
+      RE(1.80, List("of", "grew", "lawn", "gang")))),
+    ("noah wagner", Seq(
+      RE(1.82, List("her", "a", "an", "gown")),
+      RE(1.80, List("her", "a", "gown", "an")))),
+    ("ingrid bernd", Seq(
+      RE(1.71, List("in", "bred", "grind")),
+      RE(1.71, List("in", "grind", "bred")))),
   )
 
+  for ((srcText, expSeq) <- expectedGrmRed) {
+    val cfg = Configurations.grammarReduced
+
+    val rdesc = cfg.cfgRaterAi.shortDescription
+    val desc = expSeq.flatMap(re => re.words).mkString(" ")
+    val rater = new RaterAi(cfg.cfgRaterAi.cfgRaterAi())
+    val anagrams: Seq[Ana] = SolverRatedImpl(baseSolver, rater).solve(srcText).toSeq
+    for (i <- 0 to 1) {
+      ignore(s"$rdesc $srcText $i $desc") {
+        val re = expSeq(i)
+        val ana = anagrams(i)
+        ana.sentence mustBe re.words
+        ana.rate mustBe re.rating +- 0.1
+      }
+    }
+  }
+
+  val expectedPlainRated = Seq(
+    ("wolfgang wagner", Seq(
+      RE(1.82, List("of", "grew", "gang", "lawn")),
+      RE(1.80, List("of", "grew", "lawn", "gang")))),
+    ("noah wagner", Seq(
+      RE(1.82, List("her", "a", "an", "gown")),
+      RE(1.80, List("her", "a", "gown", "an")))),
+    ("ingrid bernd", Seq(
+      RE(1.71, List("in", "bred", "grind")),
+      RE(1.71, List("in", "grind", "bred")))),
+  )
+
+  for ((srcText, expSeq) <- expectedPlainRated) {
+    val cfg = Configurations.plainRated
+
+    val rdesc = cfg.cfgRaterAi.shortDescription
+    val desc = expSeq.flatMap(re => re.words).mkString(" ")
+    val rater = new RaterAi(cfg.cfgRaterAi.cfgRaterAi())
+    val anagrams: Seq[Ana] = SolverRatedImpl(baseSolver, rater).solve(srcText).toSeq
+    for (i <- 0 to 1) {
+      ignore(s"$rdesc $srcText $i $desc") {
+        val re = expSeq(i)
+        val ana = anagrams(i)
+        ana.sentence mustBe re.words
+        ana.rate mustBe re.rating +- 0.1
+      }
+    }
+  }
 
 }
